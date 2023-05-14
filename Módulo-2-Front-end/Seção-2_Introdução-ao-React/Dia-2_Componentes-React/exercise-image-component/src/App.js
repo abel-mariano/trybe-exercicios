e876a66874c01a6c5dab6c430a40d86a616fdfd6
Abel Mariano <abelwm25@gmail.com>
2023-05-14 02:01:00 -0300

    Update file

---
+++ Módulo-2-Front-end/Seção-2_Introdução-ao-React/Dia-2_Componentes-React/exercise-image-component/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from './Image';
+import staringCat from './staringCat.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <Image source={ staringCat } alternativeText="Cute cat staring" />
+    </main>
+  );
+}
+
+export default App;
