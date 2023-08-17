26c4415fc6e0fe375110b0940bb63b3b1283a58f
Abel Mariano <abelwm25@gmail.com>
2023-08-17 16:55:22 -0300

    Finalizado dia-1, seção-5

---
+++ Módulo-3-Back-end/Seção-5_Arquitetura-de-Software-Model-Service-e-Controller/Dia-1_Arquitetura-de-Software-Camada-Model/trybecar/src/models/index.js
@@ -0,0 +1,6 @@
+/* Este vai ser o único lugar do nosso código onde importamos o objeto Model direto do seu arquivo */
+const passengerModel = require('./passenger.model');
+
+module.exports = {
+  passengerModel,
+};
