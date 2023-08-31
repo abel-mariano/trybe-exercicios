021dabf32215978436da61516fe06c9b37c51b2b
Abel Mariano <abelwm25@gmail.com>
2023-08-31 18:00:28 -0300

    Finalizado dia-1, seção-6

---
+++ Módulo-3-Back-end/Seção-6_NodeJs-ORM-e-Autenticação/Dia-1_ORM-Interface-da-aplicação-com-o-banco-de-dados/app-with-sequelize/src/models/user.model.js
@@ -0,0 +1,16 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    // aqui inserimos o datatype da coluna criada
+    phoneNum: DataTypes.STRING,
+  });
+  (async () => {
+    await sequelize.sync({ force: true });
+    // As funções vão aqui
+  })();
+
+  return User;
+};
+
+module.exports = UserModel;
